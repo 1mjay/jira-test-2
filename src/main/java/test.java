c19fe1e2d0fd1963d87aa69fd055a4b5cc84ab06
1mjay <sumthq@gmail.com>
2024-11-19 13:54:39 +0900

    Test: 테스트입니다

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("지라 인텔리제이 연동 확인 완료!");
-    
+        System.out.println("커밋 메시지에 안붙인거 테스트테스트");
     }
 }
